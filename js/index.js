74d2ebed51738aeee3e7e3fd9fd36740aa10948f
Mauricio Serna <112326618+mauser78@users.noreply.github.com>
2024-08-22 16:15:10 -0500

    Trabajo en busqueda de productos.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.dir(document);
+// Identificacion al titulo de la barra  de navegación.
+const main = document.getElementsByTagName("main");
+
+let navbar = document.getElementById("navbarOne").innerText;
+
+let cardsToModify = document.getElementById("cards");
+let cards = document.createElement("div")
+cards.innerHTML = "Esto es una divición creada dinamicamente"
+// console.log(cardsToModify);
+cardsToModify.append(cards);
+let inputName = document.createElement("input");
+let inputCost = document.createElement("input");
+
+// cardsToModify.remove();
+// main[0].remove();
+
+cardsToModify.appendChild(inputName);
+cardsToModify.appendChild(inputCost);
+inputName.id = "nameProduct";
+inputCost.id = "costProduct";
+
+
+
