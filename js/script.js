c1cbfc2d92c2e4f3e1600dd2746842add8466e50
Mauricio Serna <112326618+mauser78@users.noreply.github.com>
2024-08-27 19:41:26 -0500

    avance en registrar email.

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// let formulario = document.querySelector('#registrationForm');
+
+// formulario.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     let form = e.target;
+//     console.log(form.children[0].value);
+//     console.log(form.children[1].value);
+
+//     document.getElementById('nombre').innerHTML = form.children[1].value;
+//     document.getElementById('email').innerHTML = form.children[0].value;
+
+// })
+
+let formulario = document.querySelector('#registrationForm');
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let form = e.target;
+    // console.log(form.children[0].value);
+    // console.log(form.children[1].value);
+    
+    // Acceder a los valores por el ID del input
+    let nombre = form.querySelector('#nombres').value;
+    let email = form.querySelector('#email').value;
+
+    console.log(nombre);
+    console.log(email);
+
+    // Mostrar los valores en elementos HTML con IDs 'nombre' y 'email'
+    document.getElementById('nombre').innerHTML = nombre;
+    document.getElementById('email').innerHTML = email;
+    // document.getElementById('nombre').innerHTML = form.children[0].value;
+    // document.getElementById('email').innerHTML = form.children[1].value;
+});
+
+
+
