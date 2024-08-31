13a5d70b35530de5cec32719735ba67597440708
Mauricio Serna <112326618+mauser78@users.noreply.github.com>
2024-08-31 15:03:21 -0500

    Agregar registro en el index.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.dir(document);
+let formulario = document.querySelector('#formulario');
+let nombre = document.getElementById('nombre');
+let email = document.getElementById('email');
+
+formulario.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    let form = e.target;
+    
+    const nameStorage = form.children[0].value;
+    const emailStorage = form.children[1].value;
+
+    for (let i = 0; i < localStorage.length; i++) {
+        let clave = localStorage.key(i);
+        // let clave = localStorage.key(i);
+        console.log("Clave: " + clave);
+        console.log("Valor: " + localStorage.getItem(clave));
+    }
+
+    localStorage.setItem("nombre", nameStorage);
+    localStorage.setItem("email", emailStorage);
+
+    // sessionStorage.setItem("nombre", nameStorage);
+    // sessionStorage.setItem("email", emailStorage);
+
+    nombre.innerHTML = localStorage.getItem("nombre");
+    email.innerHTML = localStorage.getItem("email");
+})
