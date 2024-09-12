1fbb24ab430ac2eb43426144389232b58a855f39
Wilder Serna <mauricio.serna78@hotmail.com>
2024-09-12 16:52:57 -0500

    Adición del boton oscuro

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,44 @@
+const cuerpo = document.body;
+const iconButton = document.getElementById("button-icon");
 const footer = document.getElementById("footer");
 const parrafoFooter = document.createElement("parrafo");
 const anioActual = new Date().getFullYear();
 
 parrafoFooter.innerHTML = "webprogrammingms | " + anioActual+" &#169";
 footer.appendChild(parrafoFooter);
+
+// Función para cambiar entre modo oscuro y claro
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonDark = document.getElementById("toggle-button");
+  
+    if (modo === 'dark-mode') {
+      cuerpo.classList.add("dark-mode");
+    }
+  
+    buttonDark.addEventListener("click", () => {
+      cuerpo.classList.toggle("dark-mode");
+      setLocalStorageModeDark()
+      updateIconButton();
+    });
+  
+    // Función para actualizar el icono del botón
+    function updateIconButton() {
+      if (cuerpo.classList.contains("dark-mode")) {
+        iconButton.classList.remove("bi-moon-fill");
+        iconButton.classList.add("bi-sun-fill");
+      } else {
+        iconButton.classList.remove("bi-sun-fill");
+        iconButton.classList.add("bi-moon-fill");
+        localStorage.removeItem("Modo");
+      }
+    }
+  
+    function setLocalStorageModeDark() {
+      if (cuerpo.classList.contains("dark-mode")) {
+        localStorage.setItem("Modo", "dark-mode");
+      } else {
+        localStorage.removeItem("Modo");
+      }
+    }
+    updateIconButton();
+  });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+// <!-- Script personalizado -->
+const cuerpo = document.body;
+const iconButton = document.getElementById("button-icon");
+let modo = localStorage.getItem("Modo");
+
 let formulario = document.querySelector('#formulario');
 let nombre = document.getElementById('nombre');
 let email = document.getElementById('email');
@@ -25,3 +30,40 @@ formulario.addEventListener("submit" , (e) => {
     nombre.innerHTML = localStorage.getItem("nombre");
     email.innerHTML = localStorage.getItem("email");
 })
+
+
+// Función para cambiar entre modo oscuro y claro
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonDark = document.getElementById("toggle-button");
+
+  if (modo === 'dark-mode') {
+    cuerpo.classList.add("dark-mode");
+  }
+
+  buttonDark.addEventListener("click", () => {
+    cuerpo.classList.toggle("dark-mode");
+    setLocalStorageModeDark()
+    updateIconButton();
+  });
+
+  // Función para actualizar el icono del botón
+  function updateIconButton() {
+    if (cuerpo.classList.contains("dark-mode")) {
+      iconButton.classList.remove("bi-moon-fill");
+      iconButton.classList.add("bi-sun-fill");
+    } else {
+      iconButton.classList.remove("bi-sun-fill");
+      iconButton.classList.add("bi-moon-fill");
+      localStorage.removeItem("Modo");
+    }
+  }
+
+  function setLocalStorageModeDark() {
+    if (cuerpo.classList.contains("dark-mode")) {
+      localStorage.setItem("Modo", "dark-mode");
+    } else {
+      localStorage.removeItem("Modo");
+    }
+  }
+  updateIconButton();
+});
